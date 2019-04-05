787d6d12015091b849de3bd0dc42ad50d03d89c6
Kenn <kenn+git@code-crew.org>
2019-04-05 11:30:05 -0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+    constructor(props) {
+        super(props);
+        this.state={
+            data: [],
+        };
+        this.loadAllTheData();
+    }
+
+    loadAllTheData(){
+        fetch('/getInfo')
+            .then(data =>
+            {
+                console.log(data);
+                // console.log(data.json());
+                return data.json();
+            })
+            .then(returnedData => this.setState({data: returnedData}));
+    }
+
+    render() {
+        // console.log(this.state.data);
+        const dataMapping = this.state.data.map((eachElement)=>{
+            return(<div>
+                <p>{eachElement.name}</p>
+                <p>{eachElement.address}</p>
+            </div>)
+        });
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <h1>Test</h1>
+          {dataMapping}
       </div>
     );
   }
